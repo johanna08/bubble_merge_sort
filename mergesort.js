baeab339694a611e2cf6f8bc2c138695c936a8d5
johanna08 <johannaperez@Johannas-MacBook.local>
2016-07-29 11:50:13 -0400

    merge answer

--- mergesort.js
+++ mergesort.js
@@ -1,11 +1,36 @@
 function split(wholeArray) {
 
   /* your code here to define the firstHalf and secondHalf arrays */
-  var firstHalf = [];
-  var secondHalf = [];
-  if ( wholeArray % 2 === 0 ){
-  	firstHalf.push()
-  }
-
+  var firstHalf = wholeArray.slice(0, wholeArray.length / 2);
+  var secondHalf = wholeArray.slice(wholeArray.length / 2);
+  
   return [firstHalf, secondHalf];
 }
+
+function mergeSort(array) {
+	//base case
+	if ( array.length === 1 ){
+		return array;
+	} else {
+		//keep splitting down to length 1
+		var splitArr = split(array);
+		return merge([mergeSort(splitArr[0]), mergeSort(splitArr[1])])
+	}
+}
+
+function merge(array){
+	var arr1 = array[0];
+	var arr2 = array[1]; 
+	var sortedArr = [];
+	//[4,5] , [1,2,3];
+	//[5] []
+	while(arr1.length && arr2.length) {
+		if (arr1[0] <= arr2[0]) {
+			sortedArr.push(arr1.shift());
+		} else {
+			sortedArr.push(arr2.shift());
+		} 
+	}
+	return sortedArr.concat(arr1.concat(arr2));
+}
+
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,5 +1,18 @@
 describe('Split Array function', function() {
   it('is able to split an array into two halves', function() {
-    // your code here 
+    expect(split( [5,4,3,2,1] ) ).toEqual( [ [5,4],[3,2,1] ] );
+
+  });
+});
+
+describe('Merge Sort', function(){
+  it('is able to merge two sorted arrays into one sorted array', function(){
+    expect(merge([[0,4] , [1,2,3]])).toEqual([0,1,2,3,4]);
+  });
+});
+
+describe('Merge Sort', function(){
+  it('is able to merge two sorted arrays into one sorted array', function(){
+    expect(mergeSort([4,3,2,1,0])).toEqual([0,1,2,3,4]);
   });
 });
